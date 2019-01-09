100903bf666ca79ea518029ea0e8000360a74b1c
Pouryakordi <33510260+Pouryakordi@users.noreply.github.com>
2019-01-09 14:56:34 +0330

    Update Form1.cs

--- Tahlil/Form1.cs
+++ Tahlil/Form1.cs
@@ -19,10 +19,6 @@ namespace Tahlil
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < 1; i++)
-            {
-
-            }
         }
     }
 }
